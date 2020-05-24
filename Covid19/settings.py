6eb1039c959d559424db6e37c84544e294b42d07
niraj <nrjadkry@gmail.com>
2020-05-24 12:20:02 +0545

    Deploying into heroku

--- Covid19/settings.py
+++ Covid19/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'p28l1uuyb)k8@ep5t#5c4nz#4s4x0#i$@(w1(not580l&xm-*n'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['niraj-covid19-data.herokuapp.com','127.0.0.1:8000']
 
 
 # Application definition
