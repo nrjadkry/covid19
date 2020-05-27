a8b6dd724a0cd66e3ca19fc64c84069608c60432
niraj <nrjadkry@gmail.com>
2020-05-27 11:30:56 +0545

    Data for all the countries are shown in Dashboard

--- livedata/views.py
+++ livedata/views.py
@@ -4,26 +4,64 @@ from django.http import JsonResponse
 
 from .forms import CovidForm
 
+from datetime import datetime
 
+import requests
 from rest_framework.views import APIView
 from rest_framework.response import Response
 
 
+today=datetime.today()
+
 # Create your views here.
 def home(request):
+
+	url1 = "https://covid-193.p.rapidapi.com/statistics"
+
+	headers1 = {
+	    'x-rapidapi-host': "covid-193.p.rapidapi.com",
+	    'x-rapidapi-key': "a37f2cca17msh79bff66e7a04eaap1de657jsn42fb0aaff772"
+	    }
+
+	response = requests.request("GET", url1, headers=headers1).json()
+
+	data=response['response']
+	lst=[]
+
+	for x in range(226):
+
+
+		d=data[x]
+
+		# print(d)
+
+		temp={
+		'country':d['country'],
+		'all':d['cases']['total'],
+		'deaths': d['deaths']['total']
+		}
+		if not (d['country']=='All' or d['country']=='Europe' or d['country']=='North-America' or d['country']=='Asia' or d['country']=='South-America' or d['country']=='Africa'):
+			lst.append(temp)
+	
+	def myFunc(e):
+		return e['all']
+
+	lst.sort(reverse=True, key=myFunc)
+	# print(lst)
+
 	form=CovidForm(request.POST)
 	if request.method == 'POST':
 		if form.is_valid:
 			country=request.POST.get('country','')
 
-			
-
+		
 	else:
 		country='Nepal'
 
 	url = "https://covid-193.p.rapidapi.com/statistics"
 
 	querystring = {"country":country}
+
 
 	headers = {
 	'x-rapidapi-host': "covid-193.p.rapidapi.com",
@@ -35,18 +73,21 @@ def home(request):
 
 	cov=data[0]
 
+
+
 	context={
+	'data':lst,
 	'country':cov['country'],
 	'all':cov['cases']['total'],
 	'recovered': cov['cases']['recovered'],
 	'deaths': cov['deaths']['total'],
 	'new': cov['cases']['new'],
 	'serious': cov['cases']['critical'],
+	'today':today
 	}
-	# print(cov)
+
 
 	return render(request, 'livedata/index.html',context)
-
 
 def get_data(request,*args,**kwargs):
 		data={
@@ -62,6 +103,29 @@ class ChartData(APIView):
 			permission_classes = []
 
 			def get(self, request, format=None):
+
+				# url = "https://data.nepalcorona.info/api/v1/covid/timeline"
+
+				# headers = {
+				# 	'x-rapidapi-host': "covid-193.p.rapidapi.com",
+				# 	'x-rapidapi-key': "be7f37114bmsh38c0486c35a5050p1bc1e5jsnf574155ad041"
+				# 	}
+				# response = requests.request("GET", url).json()
+
+				# data=response['result']
+
+				# d=data[0]
+
+				# print(data)
+
+				# data=response['response']
+
+				# print(data[0])
+
+
+
+
+
 			
 				labels=['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange']
 				default_items=[159,145,123,17]
